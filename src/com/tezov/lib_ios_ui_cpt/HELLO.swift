1552606ee99b0eaa8217380f1bf5298158ab8c46
tezov <tezov.app@gmail.com>
2023-08-20 18:34:02 +0200

    initial commit

---
+++ src/com/tezov/lib_ios_ui_cpt/HELLO.swift
@@ -0,0 +1,13 @@
+//
+//  HELLO.swift
+//  lib_ios_app_core
+//
+//  Created by prive on 20/08/2023.
+//
+
+import Foundation
+import lib_ios_ui_core
+
+public func helloUiCpt() -> String {
+    return "hello ui cpt and \(helloUiCore())"
+}
